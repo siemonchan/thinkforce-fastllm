2bfebda6371d5ce4bc6a6771c2b6fbd43756e560
siemon <siemon1996@hotmail.com>
2023-10-16 08:39:53 +0000

    update gitignore, remove unnecessary files

--- third_party/thinkforce/driver/tfacc2/result/.tfacc2.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/siemon/tfdl/Dist/NPU40T/driver/tfacc2/result/tfacc2.mod := { echo  /home/siemon/tfdl/Dist/NPU40T/driver/tfacc2/result/tfacc2.o;  echo; } > /home/siemon/tfdl/Dist/NPU40T/driver/tfacc2/result/tfacc2.mod
